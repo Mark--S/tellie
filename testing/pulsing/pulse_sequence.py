badb63b85147228d517ede9d2291f9e29fa7c5bb
EdLeming <e.leming09@googlemail.com>
2016-08-08 14:41:09 +0100

    updated serial command to properly handle upper/lower boxes in slave

--- testing/pulsing/pulse_sequence.py
+++ testing/pulsing/pulse_sequence.py
@@ -6,29 +6,34 @@ def safe_exit(sc,e):
     print "Exit safely"
     print e
     sc.stop()
+    sys.exit()
 
 if __name__=="__main__":
-    width = sys.argv[1]
-    rate = sys.argv[2]
-    number = sys.argv[3]
-    width = int(width)
-    rate = float(rate)
-    number = int(number)
-    print width,rate
-    sc = serial_command.SerialCommand("/dev/tty.usbserial-FTF5YKDL")
+    width = int(sys.argv[1])
+    delay = float(sys.argv[2])
+    number = int(sys.argv[3])
+    channel = int(sys.argv[4])
+    sc = serial_command.SerialCommand("/dev/tty.usbserial-FTGA2OCZ")
     sc.stop()
-    sc.select_channel(7)
+    sc.select_channel(channel)
+    sc.set_trigger_delay(0)
     sc.set_pulse_height(16383)
     sc.set_pulse_width(width)
-    sc.set_pulse_delay(rate)
+    sc.set_pulse_delay(delay)
     sc.set_pulse_number(number)
     try:
-        sc.fire()
-        while True:
-            pass
+        sc.fire_sequence()
+    except Exception,e:
+        safe_exit(sc,e)
+
+    mean = None
+    try:
+        print "Waiting for sequence to finish..."
+        while (mean == None):
+            mean, rms, chan = sc.read_pin_sequence()
     except Exception,e:
         safe_exit(sc,e)
     except KeyboardInterrupt:
-        safe_exit(sc,"keyboard interrupt")
-        
-        
+        safe_exit(sc, "keyboard interrupt")
+
+    print "\nPin: %s \nrms: %s\n" % (mean, rms)
