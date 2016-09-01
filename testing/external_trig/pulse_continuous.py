fc6416c880189066b8ad3e3895b4968a1ad7ea60
Mark Stringer <ms711@feynman.cm.cluster>
2016-09-01 20:14:34 +0100

    Underground USB changes@

--- testing/external_trig/pulse_continuous.py
+++ testing/external_trig/pulse_continuous.py
@@ -16,7 +16,7 @@ if __name__=="__main__":
     channel = int(channel)
     print width#, rate
     sc = serial_command.SerialCommand("/dev/tty.usbserial-FTE3C0PG")
-    #sc.stop()
+    sc.stop()
     sc.select_channel(channel)
     sc.set_pulse_height(16383)
     sc.set_pulse_width(width)
