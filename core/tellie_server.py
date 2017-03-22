7073f413c375405ba395962988be4543a3730ea8
mnirkko <m.nirkko@sussex.ac.uk>
2017-03-22 18:01:40 +0000

    Added a few lines to check if chars written are in UTF-8 or HEX. Cannot yet parse unicode.

--- core/tellie_server.py
+++ core/tellie_server.py
@@ -55,64 +55,6 @@ class ThreadException(Exception):
         Exception.__init__(self, error)
 
 
-#class ParametersClass(object):
-#    """Class to emulate common/parameters """
-#
-#    def __init__(self):
-#        # Some init stuff
-#        self._something = 0;
-#
-#    def pulse_number(self, number):
-#        adjusted = False
-#        if type(number)!=int:
-#            raise Exception("PN must be an integer")
-#        if number > p._max_pulse_number:
-#            raise Exception("PN must be < %d.  You set %d" % (p._max_pulse_number, number))
-#        #number = max_pulse_number
-#        #adjusted = True
-#        hi = -1
-#        lo = -1
-#        diff = 100000 # bigger than max pn
-#        for i in range(1, 256):
-#            # assume hi is i
-#            lo_check = number/i
-#            if lo_check > p._max_pulse_number_lower:
-#                lo_check = p._max_pulse_number_lower
-#            check = i * lo_check
-#            if math.fabs(check - number) < diff:
-#                diff = math.fabs(check - number)
-#                hi = i
-#                lo = lo_check
-#            if check == number:
-#                break
-#        actual_par = hi * lo
-#        if actual_par != number:
-#            adjusted = True
-#        return adjusted, actual_par, hi, lo
-#
-#    def trigger_delay(self, delay):
-#        adjusted = False
-#        delay = float(delay)
-#        if delay > p._max_trigger_delay or delay < 0:
-#            raise Exception("TD must be >%s and <%s" % (0, p._max_trigger_delay))
-#        parameter = int(round(delay)/5)
-#        adj_delay = parameter * 5
-#        if delay != adj_delay:
-#            adjusted = True
-#        return adjusted, adj_delay, parameter
-#
-#    def fibre_delay(self, delay):
-#        adjusted = False
-#        delay = float(delay)
-#        if delay > p._max_fibre_delay or delay < 0:
-#            raise Exception("FD must be >%s and <%s" % (0, p._max_fibre_delay))
-#        parameter = int(round(delay * 4.))
-#        adj_delay = float(parameter) / 4.
-#        if delay != adj_delay:
-#            adjusted = True
-#        return adjusted, adj_delay, parameter
-
-
 class SerialCommand(object):
     """Contains a serial command object.
     """
@@ -248,14 +190,22 @@ class SerialCommand(object):
         if type(command) is not list:
             raise TellieException("Command is not a list: %s %s" % (command, type(command)))
         try:
-            for c in command:
-                self.log_phrase("Writing char %s" % c, 0, _snotDaqLog)
-                bytesWritten = self._serial.write(c)
-                self.log_phrase("Written char %s" % c, 0, _snotDaqLog)
-                self.log_phrase("Bytes Written %d" % bytesWritten, 0, _snotDaqLog)
+            for char in command:
+                data = list(char)
+                for c in data:
+                    try:    c = c.encode('utf8')
+                    except: c = int(c.encode('hex'),16)
+                    if c is '\x00': c = 'NULL'
+                    self.log_phrase("Writing char %s" % c, 0, _snotDaqLog)
+                bytesWritten = self._serial.write(char)
+                for c in data:
+                    try:    c = c.encode('utf8')
+                    except: c = int(c.encode('hex'),16)
+                    if c is '\x00': c = 'NULL'
+                    self.log_phrase("Written char %s" % c, 0, _snotDaqLog)
+                self.log_phrase("Bytes written %d" % bytesWritten, 0, _snotDaqLog)
                 time.sleep(sleep_after_command)
         except Exception as e:
-            #print e
             raise TellieException("Lost connection with TELLIE hardware! Re-set server")
 
         if not buffer_check: # assume returns same as input
